b2cf379dbe0ce76523c276b1bee3cbfedb4fcfa1
Turgho <victor.hugo3692111@gmail.com>
2026-05-17 02:14:04 -0300

    feat: Sets up core app routing with guards and lazy loading
    Implements the initial routing structure for the application, including protected routes.
    
    Introduces `authGuard` and `guestGuard` to control access to routes, redirecting users based on their authentication status. Configures lazy loading for the `LoginComponent` and `DashboardComponent` to optimize bundle size and improve initial load times.
    
    Also defines a default redirect from the root path to login and a fallback route for unmatched paths.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,30 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { Login } from './features/auth/login/login';
+import { authGuard, guestGuard } from './core/guards/auth';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        component: Login,
-    }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+
+  // Rotas públicas (bloqueadas se já estiver logado)
+  {
+    path: 'login',
+    canActivate: [guestGuard],
+    loadComponent: () =>
+      import('./features/auth/login/login').then(m => m.LoginComponent),
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/dashboard/dashboard/dashboard').then(m => m.DashboardComponent),
+  },
+  // Fallback
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
 ];
